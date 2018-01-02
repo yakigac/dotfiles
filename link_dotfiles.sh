06ca2bfbfbc3314cc572f3d8a22b8368908cc5f1
Ryohei Kuroki <ryohei.kuroki@gmail.com>
2018-01-02 23:54:46 +0900

    Add replacing for zsh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,5 +1,11 @@
-# !/bin/sh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+# !/bin/zsh
+# Replace .vimrc, .gitconfig and config files for zsh
+# You should backup these files before
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
+setopt EXTENDED_GLOB
+for rcfile in ~/dotfiles/prezto/runcoms/^README.md(.N); do
+  ln -sf ${rcfile} ~/.${rcfile:t}
+done
